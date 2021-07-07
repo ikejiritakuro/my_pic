86a1bd21e2d4dcbe42734ab84cac903889ff678f
ikejiritakuro <talangchikao165@gmail.com>
2021-07-07 15:02:19 +0900

    実装

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,18 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params) 
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+end

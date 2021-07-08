cfab696199fcfbe23c4824561cfad0036cdb58a1
ikejiritakuro <talangchikao165@gmail.com>
2021-07-08 11:16:35 +0900

    実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'tweets#index' 
   resources :tweets do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show
 end

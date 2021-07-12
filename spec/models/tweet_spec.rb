ad987d0a316eeae8a870c26bb42cc7b05318e67c
ikejiritakuro <talangchikao165@gmail.com>
2021-07-12 14:27:47 +0900

    エラー文日本語

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -15,17 +15,17 @@ RSpec.describe Tweet, type: :model do
       it 'テキストが空では投稿できない' do
         @tweet.text = ''
         @tweet.valid?
-        expect(@tweet.errors.full_messages).to include("Text can't be blank")
+        expect(@tweet.errors.full_messages).to include("テキストを入力してください")
       end 
       it '画像が空では登録できない' do
         @tweet.image = nil
         @tweet.valid?
-        expect(@tweet.errors.full_messages).to include("Image can't be blank")
+        expect(@tweet.errors.full_messages).to include("画像を入力してください")
       end
       it 'ユーザーが紐付いていなければ投稿できない' do
         @tweet.user = nil
         @tweet.valid?
-        expect(@tweet.errors.full_messages).to include("User must exist")
+        expect(@tweet.errors.full_messages).to include("Userを入力してください")
       end
     end
   end
